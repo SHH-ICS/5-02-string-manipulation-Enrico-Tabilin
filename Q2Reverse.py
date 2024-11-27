4219e59cb36225d3c6837a8375f11735b459c79e
Enrico Tabilin <enrico.tabilin@stu.ocsb.ca>
2024-11-27 15:17:40 +0000

    Update Q2Reverse.py (I think I did it right?)

--- Q2Reverse.py
+++ Q2Reverse.py
@@ -1,7 +1,5 @@
 # Create a program that will accept a word and output the word one letter at a time in reverse.
 print('This program reverses a word or a sentence you enter, one letter at a time.')
 txt=input()
-backwards=''
 for x in range(len(txt)):
-    backwards=backwards+txt[len(txt)-1-x]
-    print(backwards)
+    print(txt[len(txt)-1-x])
